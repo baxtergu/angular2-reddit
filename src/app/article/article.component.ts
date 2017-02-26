ac9447c4c36132d6367e79a4a80817e047e93897
baxtergu <baxtergu@gmail.com>
2017-02-26 23:42:44 +0800

    练习完成

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,13 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
+  host: {
+    class: 'row'
+  }
 })
 export class ArticleComponent implements OnInit {
+  @Input() article: Article;
 
   constructor() { }
+
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown(): boolean {
+    this.article.voteDown();
+    return false;
+  }
 
   ngOnInit() {
   }
